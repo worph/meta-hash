257db022f8d59b40d4050eba8b645e4bf61575e9
Pierre-Henri <>
2024-04-18 17:44:48 +0200

    ignore the dist

--- dist/index-interface.js
+++
@@ -1,4 +0,0 @@
-import './chunk-XCJZALP2.js';
-export { CID_ALGORITHM, CID_ALGORITHM_CODES, CID_ALGORITHM_NAMES } from './chunk-HTZ763NS.js';
-//# sourceMappingURL=out.js.map
-//# sourceMappingURL=index-interface.js.map
--- dist/worker.js
+++
@@ -1,5 +0,0 @@
-export { computeCIDs as default } from './chunk-LTWPBXM5.js';
-import './chunk-SV45D2HN.js';
-import './chunk-HTZ763NS.js';
-//# sourceMappingURL=out.js.map
-//# sourceMappingURL=worker.js.map
